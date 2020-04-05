1ad1032fbf661f8d038388ce8d8d0cde59ffa755
sourc3w <dolardedoler@gmail.com>
2020-04-05 14:37:04 +0200

    Primera Subida

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTime;
+    public int score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<EnemyController>().currState = EnemyState.Die;
+            //col.gameObject.GetComponent<EnemyController>().Death();
+            Destroy(gameObject);
+            score += 1;
+            Debug.Log("Score: " + score);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 movement;
+    private Vector2 velocity;
+    public float speed = 2f;
+    new Rigidbody2D rigidbody;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    private float lastFire;
+    public float fireDelay;
+    public int health = 3;
+    Renderer rend;
+    Color c;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rend = GetComponent<Renderer>();
+        c = rend.material.color;
+    }
+
+    void Update()
+    {
+        float horMov = Input.GetAxisRaw("Horizontal");
+        float verMov = Input.GetAxisRaw("Vertical");
+        float horShoot = Input.GetAxisRaw("HorizontalShooting");
+        float verShoot = Input.GetAxisRaw("VerticalShooting");
+
+        if((horShoot != 0 || verShoot != 0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(horShoot, verShoot);
+            lastFire = Time.time;
+        }
+
+        rigidbody.velocity = new Vector2(horMov * speed, verMov * speed);
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2
+            (
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed
+            );
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if((col.gameObject.tag == "Enemy") && (health > 0))
+        {
+            health -= 1;
+           StartCoroutine("Invulnerable");
+        }
+        else if(health == 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+    IEnumerator Invulnerable()
+    {
+        Physics2D.IgnoreLayerCollision(10, 9, true);
+        c.a = 0.6f;
+        rend.material.color = c;
+        yield return new WaitForSeconds(1.5f);
+        Physics2D.IgnoreLayerCollision(10, 9, false);
+        c.a = 1f;
+        rend.material.color = c;
+
+    }
+}
